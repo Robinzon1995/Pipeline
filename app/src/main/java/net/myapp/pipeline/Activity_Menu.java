5e05e29f26ea1b17a2f850f01cbc6a0785cdf439
ExE <wanhels@outlook.com>
2017-08-22 10:56:21 -0700

    first commit

---
+++ app/src/main/java/net/myapp/pipeline/Activity_Menu.java
@@ -0,0 +1,12 @@
+package net.myapp.pipeline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
